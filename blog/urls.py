50773b0353c4b7a885cfb95ccab40393f7adc9fe
serega-miha <serega.miha@gmail.com>
2022-02-18 15:24:08 +0400

    перед базовыми классами и вьюхами

--- blog/urls.py
+++ blog/urls.py
@@ -26,10 +26,3 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path("ckeditor/", include('ckeditor_uploader.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('myblog/', include('myblog.urls')),
-    path('admin/', admin.site.urls),
-]
--- myblog/admin.py
+++ myblog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'url': ('title',)}
+
+admin.site.register(Post, PostAdmin)
